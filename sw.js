20c90cd76d58019269748c967d99391fe98cc7b1
aguadulcexpress26-gif <aguadulcexpress26@gmail.com>
2026-04-01 01:14:01 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+// Service Worker para Aguadulce Express
+const CACHE_NAME = 'aguadulce-v1';
+
+self.addEventListener('install', (e) => {
+  // Instalación del Service Worker
+});
+
+self.addEventListener('fetch', (e) => {
+  // Manejo de peticiones para que la app sea fluida
+});

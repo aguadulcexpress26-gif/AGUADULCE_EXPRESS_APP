3c198f3ae0cb0e8e54786632ddf632e61c4bd67f
aguadulcexpress26-gif <aguadulcexpress26@gmail.com>
2026-05-20 16:31:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,19 @@
-// Service Worker para Aguadulce Express
-const CACHE_NAME = 'aguadulce-v1';
+const CACHE_NAME = 'aguadulce-v2';
 const urlsToCache = [
   './',
   './index.html',
-  './cocinaxpress.html',
+  './COCINA.html',
+  './repartidor_prueba.html',
   './manifest.json',
   'https://i.postimg.cc/mZwjLXxw/Logo-Aguadulce-Express.jpg'
 ];
 
-// Instalación del Service Worker y almacenamiento en caché
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Activación y limpieza de cachés antiguas
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Manejo de peticiones (Estrategia: Network First)
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))

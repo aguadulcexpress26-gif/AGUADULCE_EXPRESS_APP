6c96451d234ec85d2604610a6581c8ae4523e2ba
aguadulcexpress26-gif <aguadulcexpress26@gmail.com>
2026-05-27 12:39:27 +0200

    Create sw-cocina.js

---
+++ cocina/sw-cocina.js
@@ -0,0 +1,31 @@
+// sw-cocina.js - Service Worker para la cocina
+const CACHE_NAME = 'aguadulce-cocina-v1';
+
+const urlsToCache = [
+  '/AGUADULCE_EXPRESS_APP/cocina/cocina.html',
+  '/AGUADULCE_EXPRESS_APP/cocina/manifest-cocina.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache).catch(err => console.warn(err)))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('script.google.com')) return;
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => key !== CACHE_NAME && caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
